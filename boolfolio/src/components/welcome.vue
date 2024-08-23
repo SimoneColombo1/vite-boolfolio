af278b487f39ffa40d5ca77c5e44dc71243c20fe
SimoneColombo1 <simcolombo2@gmail.com>
2024-08-23 20:36:07 +0200

    Aggiunta la sezione welcome in home page

---
+++ boolfolio/src/components/welcome.vue
@@ -0,0 +1,26 @@
+<script>
+export default {};
+</script>
+<template>
+  <section class="welcome">
+    <h1>
+      <img src="../assets/img/pngwing.com.png" />
+      Welcome to Project Manager 1.0
+    </h1>
+  </section>
+</template>
+
+<style scoped lang="scss">
+.welcome {
+  background-color: #0d6af2;
+
+  padding: 6rem;
+  h1 {
+    font-size: 3rem;
+    img {
+      filter: brightness(0) saturate(100%) invert(10%) sepia(9%) saturate(854%)
+        hue-rotate(169deg) brightness(98%) contrast(90%);
+    }
+  }
+}
+</style>
--- boolfolio/src/pages/AppHome.vue
+++ boolfolio/src/pages/AppHome.vue
@@ -1,6 +1,25 @@
 <script>
-export default {};
+import welcome from "../components/welcome.vue";
+import AboutUs from "../components/AboutUs.vue";
+import reviews from "../components/reviews.vue";
+import YourProjects from "../components/YourProjects.vue";
+import Faq from "@/components/Faq.vue";
+export default {
+  components: {
+    welcome,
+    AboutUs,
+    Faq,
+    reviews,
+    YourProjects,
+  },
+};
 </script>
-<template></template>
+<template>
+  <welcome />
+  <AboutUs />
+  <YourProjects />
+  <reviews />
+  <Faq />
+</template>
 
 <style scoped lang="scss"></style>
