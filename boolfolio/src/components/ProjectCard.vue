27b0ce7bf48e32aef089a4df9c40f269de0e3965
SimoneColombo1 <simcolombo2@gmail.com>
2024-08-23 15:44:14 +0200

    Aggiunto il carosello in all projects

--- boolfolio/src/components/ProjectCard.vue
+++ boolfolio/src/components/ProjectCard.vue
@@ -5,6 +5,7 @@ export default {
     return {
       projects: [],
       currentPage: 1,
+      indexScroll: 0,
     };
   },
   methods: {
@@ -27,6 +28,29 @@ export default {
           // always executed
         });
     },
+    scrollLeft() {
+      this.indexScroll--;
+      if (this.indexScroll >= 0) {
+        this.$refs.scrollable.scrollBy({ left: -478, behavior: "smooth" });
+        console.log(this.indexScroll);
+      } else {
+        this.$refs.scrollable.scrollBy({ left: 3500, behavior: "smooth" });
+        this.indexScroll = this.projects.length - 2;
+        console.log(this.indexScroll);
+      }
+    },
+    scrollRight() {
+      this.indexScroll++;
+      if (this.indexScroll < this.projects.length - 2) {
+        this.$refs.scrollable.scrollBy({ left: 478, behavior: "smooth" });
+
+        console.log(this.indexScroll);
+      } else {
+        this.$refs.scrollable.scrollBy({ left: -3500, behavior: "smooth" });
+        this.indexScroll = 0;
+        console.log(this.indexScroll);
+      }
+    },
   },
   created() {
     this.getProject();
@@ -34,7 +58,13 @@ export default {
 };
 </script>
 <template>
-  <div class="container card-container">
+  <a @click="scrollLeft()" class="left">
+    <img src="../assets/img/leftarrow.png" />
+  </a>
+  <a @click="scrollRight()" class="right">
+    <img src="../assets/img/rightarrow.png" />
+  </a>
+  <div class="container card-container" ref="scrollable">
     <router-link
       v-for="project in projects"
       :to="{ name: 'SingleProject', params: { id: project.id } }"
@@ -50,30 +80,44 @@ export default {
         <p>Data fine del progetto: {{ project.data_fine }}</p>
         <p v-if="project.completato > 0">Completato: Si</p>
         <p v-else>Completato: No</p>
-      </div>
-    </router-link>
-    <a
-      href=""
-      class="btn btn-primary btn-lg"
-      @click.prevent="getProject(currentPage + 1)"
-    >
-      Load more
+      </div> </router-link
+    ><a href="" class="" @click.prevent="getProject(currentPage + 1)">
+      <img src="../assets/img/plus-sign.png" />
     </a>
   </div>
 </template>
 
 <style lang="scss">
+img {
+  width: 4rem;
+  height: 4rem;
+}
+
+.left {
+  position: absolute;
+  bottom: 28rem;
+  left: 4rem;
+}
+.right {
+  position: absolute;
+  bottom: 28rem;
+  right: 4rem;
+}
 .card-container {
   margin-top: 1rem;
   display: flex;
   flex-direction: row;
-  flex-wrap: wrap;
-  justify-content: center;
+  overflow: hidden;
+  width: 100vw;
 
   div {
-    width: 23rem;
+    width: 18rem;
+    height: 25rem;
     margin: 1rem;
     padding: 1rem;
+    &:hover {
+      background-color: lightgray;
+    }
 
     h3 {
       color: #3787fd;
@@ -81,6 +125,11 @@ export default {
   }
   a {
     margin: 1rem;
+
+    img {
+      border: 1px black solid;
+      margin-top: 10rem;
+    }
   }
 }
 </style>
